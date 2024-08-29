682c1fee432ac84e364571eb1faffb3c9e3dad21
Alexsander Gutierrez Gonçalves <38378566+Alex5ander@users.noreply.github.com>
2024-08-29 16:15:58 -0300

    fixed

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ self.addEventListener('install', (e) => {
       const cache = await caches.open(CACHE_NAME);
       await cache.addAll([
         "/",
+        "/index.html",
         "/images/icons/icon-72x72.png",
         "/images/icons/icon-96x96.png",
         "/images/icons/icon-128x128.png",
@@ -13,7 +14,6 @@ self.addEventListener('install', (e) => {
         "/images/icons/icon-192x192.png",
         "/images/icons/icon-384x384.png",
         "/images/icons/icon-512x512.png",
-        "/index.html",
         "/run-impulse.js",
         "/manifest.json",
         "/edge-impulse-standalone.wasm",
@@ -24,28 +24,22 @@ self.addEventListener('install', (e) => {
 });
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
+    caches.keys().then((keys) => {
+      return Promise.all(keys
+        .filter(key => key != CACHE_NAME))
+        .map(key => caches.delete(key));
     })
   );
 });
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(e.request);
-      const cache = await caches.open(CACHE_NAME);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })(),
+    caches.match(e.request).then(cache => {
+      return cache || fetch(e.request).then(response => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request.url, response.clone());
+          return response;
+        })
+      })
+    })
   );
 });
