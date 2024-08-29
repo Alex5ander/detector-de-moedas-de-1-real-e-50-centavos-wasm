adf9f4fed9025bcbba54145597eecbe48fc938dc
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2024-08-29 17:06:13 -0300

    test

--- sw.js
+++ sw.js
@@ -4,19 +4,19 @@ self.addEventListener('install', (e) => {
     (async () => {
       const cache = await caches.open(CACHE_NAME);
       await cache.addAll([
-        "/",
-        "/index.html",
-        "/images/icons/icon-72x72.png",
-        "/images/icons/icon-96x96.png",
-        "/images/icons/icon-128x128.png",
-        "/images/icons/icon-144x144.png",
-        "/images/icons/icon-152x152.png",
-        "/images/icons/icon-192x192.png",
-        "/images/icons/icon-512x512.png",
-        "/run-impulse.js",
-        "/manifest.json",
-        "/edge-impulse-standalone.wasm",
-        "/edge-impulse-standalone.js"
+        "./",
+        "./index.html",
+        "./images/icons/icon-72x72.png",
+        "./images/icons/icon-96x96.png",
+        "./images/icons/icon-128x128.png",
+        "./images/icons/icon-144x144.png",
+        "./images/icons/icon-152x152.png",
+        "./images/icons/icon-192x192.png",
+        "./images/icons/icon-512x512.png",
+        "./run-impulse.js",
+        "./manifest.json",
+        "./edge-impulse-standalone.wasm",
+        "./edge-impulse-standalone.js"
       ]);
     })()
   );
