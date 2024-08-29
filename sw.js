a0e398b0b464932fa92832c5341896f6ce9818e0
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2024-08-28 23:25:05 -0300

    improved system cached

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-var CACHE_NAME = "application_cache";
+var DB_NAME = "application_cache_db";
+var DB_VERSION = 1;
+var STORE_NAME = "resources";
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
     (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      await cache.addAll([
+      const db = await openDatabase();
+      const urlsToCache = [
         "/",
         "/images/icons/icon-72x72.png",
         "/images/icons/icon-96x96.png",
@@ -18,34 +21,86 @@ self.addEventListener('install', (e) => {
         "/manifest.json",
         "/edge-impulse-standalone.wasm",
         "/edge-impulse-standalone.js"
-      ]);
+      ];
+
+      for (const url of urlsToCache) {
+        const response = await fetch(url);
+        await storeInDatabase(db, url, response);
+      }
     })()
   );
 });
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
+
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
+      const db = await openDatabase();
+      const cachedResponse = await getFromDatabase(db, e.request.url);
+      if (cachedResponse) {
+        console.log(`[Service Worker] Serving from IndexedDB: ${e.request.url}`);
+        return new Response(cachedResponse.body, {
+          headers: cachedResponse.headers
+        });
       }
+
       const response = await fetch(e.request);
-      const cache = await caches.open(CACHE_NAME);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+      await storeInDatabase(db, e.request.url, response.clone());
       return response;
-    })(),
+    })()
   );
 });
+
+// Abre uma conexão com o IndexedDB
+async function openDatabase() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      db.createObjectStore(STORE_NAME, { keyPath: 'url' });
+    };
+
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+}
+
+// Armazena uma resposta no IndexedDB
+async function storeInDatabase(db, url, response) {
+  const data = {
+    url: url,
+    body: await response.blob(),
+    headers: [...response.headers]
+  };
+
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(STORE_NAME, 'readwrite');
+    const store = transaction.objectStore(STORE_NAME);
+    const request = store.put(data);
+
+    request.onsuccess = () => resolve();
+    request.onerror = () => reject(request.error);
+  });
+}
+
+// Recupera uma resposta do IndexedDB
+async function getFromDatabase(db, url) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(STORE_NAME, 'readonly');
+    const store = transaction.objectStore(STORE_NAME);
+    const request = store.get(url);
+
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+}
